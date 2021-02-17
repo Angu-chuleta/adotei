bfe60a33bcfb548e6a1a1c6ed477555e737489d8
David Vilaça <vilacapdavid@gmail.com>
2021-02-17 16:32:46 -0300

    feat(api): add institution routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import utilsRoutes from './utils.routes';
 import pet from './pet';
+import institution from './institution';
 
 const routes = Router();
 
 routes.use(utilsRoutes);
 routes.use(pet);
+routes.use(institution);
 
 export default routes;
--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,8 +1,12 @@
 import { PetService } from './pet.service';
+import { InstitutionService } from './institution.service';
 
 const petService = new PetService();
+const institutionService = new InstitutionService();
 
 export {
   petService,
   PetService,
+  institutionService,
+  InstitutionService,
 };
